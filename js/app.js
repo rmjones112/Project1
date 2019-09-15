9b5935f0ae18b722781c8f4ef3b560511032b897
Roger Zavala <Rogeliozavala15@gmail.com>
2019-09-14 23:24:33 -0700

    added ajax call for open weather api and event bright api

--- js/app.js
+++ js/app.js
@@ -1,7 +1,44 @@
+
+var yelpURL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=seattle&categories=italian'
+
+var weatherURL = `https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2d57a57cff835d1bdd699a50fab1b250`
+
+var eventBrightURL =`https://cors-anywhere.herokuapp.com/https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=KINSGLZ2GOYY4ADBGQY&redirect_uri=https://www.eventbrite.com/oauth/VD4EN65PKYP7BLSJ4XPA&`
+
+// grabs information about food from "yelp"
 $.ajax({
-    url:`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2d57a57cff835d1bdd699a50fab1b250`,
-    method:"GET"
+    url: yelpURL,
+    method: "GET",
+    beforeSend: function (xhr) {
+        xhr.setRequestHeader("Authorization", "Bearer" + "XiFkJFuigGVABcZ-Xd8w2-i_UCfLMIIRPf6miBBNlbfUDlp7k-Z3tACaYHnJuOX97DzcFH2LrFn1c2-F3A2RxoKXDMyw0tLiW9jNPBeXpdaKIiWxSgmx2jZjfkl5XXYx");
+    },
 
 }).then(function(response){
+    console.log("Yelp");
+    console.log(response);
+
+});
+// grabs information about weather from "open weather"
+$.ajax({
+    url: weatherURL,
+    method: "GET"
+
+}).then(function (response) {
+    console.log("weather")
     console.log(response);
 });
+
+// // grabs information from "event bright" on events happening 
+// $.ajax({
+//     url: eventBrightURL,
+//     method: "GET",
+//     beforeSend: function (xhr) {
+//         xhr.setRequestHeader("Authorization: Bearer GAFDAJ43Z42P2Z5NMDAQ");
+//     },
+    
+
+// }).then(function(response){
+//     console.log("Event Bright")
+//     console.log(response);
+
+// });
