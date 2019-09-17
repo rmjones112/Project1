8a27d74a579f89eb173d235f7333fde9a3f6b207
Nicole Roberts <nicolerobertsb@gmail.com>
2019-09-17 13:31:48 -0700

    changed names

--- js/app.js
+++ js/app.js
@@ -6,9 +6,9 @@ var weatherURL = `https://cors-anywhere.herokuapp.com/api.openweathermap.org/dat
 var eventBrightURL = `https://cors-anywhere.herokuapp.com/https://www.eventbriteapi.com/v3/events/search?location.address=seattle&location.within=10km&expand=venue`
 
 // grabs information from "Yelp"
-function grabRestaurant() {
+function grabRestaurantInfo() {
     // link to yelp api
-    var yelpURL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=seattle&categories=pizza&radius=16094&rating=3.5&price=2,3&limit=5`;
+    var yelpURL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=seattle&radius=16094&rating=5&price=2,3&limit=5`;
 
     $.ajax({
         url: yelpURL,
@@ -27,14 +27,37 @@ function grabRestaurant() {
         console.log("Image of the restaurant: " + response.businesses[0].img_url);
         console.log(response);
 
+        //Loops through yelps businesses array 
+        for (let i = 0; i < 5; i++) {
+            //creates a new div to hold restaurant info
+            var restaurantDiv = $("<div>");
+            //image element to store businesses image url
+            var restaurantImage = $(`<img src="${response.businesses[i].image_url}">`)
+            console.log(restaurantImage)
+            //Gets restaurant name
+            restaurantDiv.append("Restaurant Name: " + response.businesses[i].name);
+            // Gets the rating of the restaurant 
+            restaurantDiv.append(" Rating: " + response.businesses[i].rating);
+            // Gets restaurant price 
+            restaurantDiv.append(" Price: " + response.businesses[i].price);
+            //adds restaurantImage to restaurant info
+            restaurantDiv.append(restaurantImage);
+
+            $("#display-food").prepend(restaurantDiv);
+
+            // console.log(response.businesses[i]);
+        }
 
     });
 
-    // var foodDiv = $("<div class=display-food>");
-    // foodDiv.append(response.business[0].name)
 
 
 }
+
+
+grabRestaurantInfo();
+
+
 // grabs information about weather from "open weather"
 $.ajax({
     url: weatherURL,
@@ -45,7 +68,7 @@ $.ajax({
     console.log(response);
 });
 
-// grabRestaurant();
+
 // grabs information from "event bright" on events happening 
 $.ajax({
     url: eventBrightURL,
