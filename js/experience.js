72e0a61ddc01c07b391986cda4a5d310e073f215
George Wyscaver <gwyscaver3@gmail.com>
2019-09-18 22:22:07 -0700

    added jquery to clear previous search info in my event listener for experiences

--- js/experience.js
+++ js/experience.js
@@ -12,18 +12,19 @@ function grabExperiences(location){
       xhr.setRequestHeader ("Authorization", "Bearer " + "5g5JBhiRRQYPP_XdUOB0yplJ7sMeepIg7suBSMb8ZgpEAmwGuGAO3--CzOc-3w0Zx35sw19opkOG02GSGDexwEMf0h5USlpO1ivs3niI9aL4A-UfrOzFg0BET6N6XXYx" );
   },
 
-      }).then(function(response){
-        console.log("Yelp")
-        console.log(response);
+      }).then(function(response) { 
+        // console.log("Yelp")
+        // console.log(response);
         //retrieves business name
-        console.log("name of  experience: " + response.businesses[0].name);
-        console.log("Price of the experience: " + response.businesses[0].price);
-        console.log("Rating of the experience: " + response.businesses[0].rating);
-        console.log("link to yelp review of the experience: " + response.businesses[0].url);
-        console.log("Image of the experience: " + response.businesses[0].img_url);
+        // console.log("name of  experience: " + response.businesses[0].name);
+        // console.log("Price of the experience: " + response.businesses[0].price);
+        // console.log("Rating of the experience: " + response.businesses[0].rating);
+        // console.log("link to yelp review of the experience: " + response.businesses[0].url);
+        // console.log("Image of the experience: " + response.businesses[0].img_url);
         // $("#portfolioModal5 #experienceContent").html(JSON.stringify(response))
         var container = $("<div>")
         var gotFancy = false
+        var gotCasual = false
         $.each(response.businesses, function(key,val){ 
             $("<a target='_blank' href = '" + val.url + "'>").append($("<h3>" + val.name + "</h3>")).appendTo(container)
             $("<h3>" + val.location.address1 + "</h3>").appendTo(container)
@@ -37,9 +38,14 @@ function grabExperiences(location){
                addPackingItem("Fancy clothes")
                gotFancy = true
            }
+           if (val.price.length >=2 && !gotCasual){
+                addPackingItem("Casual clothing")
+                gotCasual = true
+        }
 
         })
-        container.appendTo($("#portfolioModal5 #experienceContent"))
+        container.appendTo("#portfolioModal5 #experienceContent")
+        
       });
-    }  
+    }
 // grabExperiences();
